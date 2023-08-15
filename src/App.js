c785699b9f23a1bdfb893b66499149de485e5354
darkos11 <gouga.11@live.com>
2023-08-15 19:49:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menubar from './components/Menubar';
+
+function App() {
+  return (
+    <Menubar/>
+    
+    
+);
+}
+
+export default App;
